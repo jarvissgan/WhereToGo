84e26077cb5e1534227d78cafc6902a937375d2a
LegallyAlive <jarvisgan@gmail.com>
2022-06-15 14:54:18 +0800

    google places implementation

---
+++ lib/location_service.dart
@@ -0,0 +1,42 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class LocationService {
+  /*
+  location service uses google places api to extract information
+  about a location from user input
+  */
+  final String key = 'AIzaSyCalXPvIMk2Vh4ypx4FuDcKPsQQUqLwwAs';
+
+  Future<String> getPlaceID(String input) async {
+    //gets place id of location, to be used in getPlace()
+    final String url =
+        'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
+
+    var response = await http.get(Uri.parse(url));
+    var json = convert.jsonDecode(response.body);
+
+    //retrieves response from api and returns a placeid as String
+    var placeId = json['candidates'][0]['place_id'] as String;
+    //candidates refers to most likely matches
+    //TODO: add autocomplete for location & generate listview when search is complete
+
+    print(placeId);
+    return placeId;
+  }
+
+  Future<Map<String, dynamic>> getPlace(String input) async {
+    //uses getPlaceId to get location details from google api
+    final placeID = await getPlaceID(input);
+    String url =
+        'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeID&key=$key';
+
+    var response = await http.get(Uri.parse(url));
+    var json = convert.jsonDecode(response.body);
+
+    var results = json['result'] as Map<String, dynamic>;
+
+    print(results);
+    return results;
+  }
+}
--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jarlist/location_service.dart';
 
 class AddScreen extends StatefulWidget {
   @override
@@ -6,19 +7,33 @@ class AddScreen extends StatefulWidget {
 }
 
 class _AddScreenState extends State<AddScreen> {
+  TextEditingController _searchController = TextEditingController();
+  static const String googleApiKey = 'AIzaSyCalXPvIMk2Vh4ypx4FuDcKPsQQUqLwwAs';
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Row(
         //TODO: complete add screen
         children: [
-          TextField(
-            // controller: _controller,
-            decoration: InputDecoration(
-              labelText: 'Search',
-              prefixIcon: Icon(Icons.search),
-            ),
-          ),
+          Expanded(
+              child: TextFormField(
+            controller: _searchController,
+            textCapitalization: TextCapitalization.words,
+            decoration: InputDecoration(hintText: 'Search'),
+            onChanged: (value) {
+              if (value != '') {
+                print(value);
+              } else {
+                value = '';
+              }
+            },
+          )),
+          IconButton(onPressed: () {
+            LocationService().getPlace(_searchController.text).then((value) {
+              print(value);
+            });
+          }, icon: Icon(Icons.search)),
         ],
       )
     ]);
